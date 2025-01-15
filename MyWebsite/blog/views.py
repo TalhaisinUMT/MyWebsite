08740e943726aad64363f19965530d3e1bf82d4f
TalhaisinUMT <160135376+TalhaisinUMT@users.noreply.github.com>
2025-01-15 14:49:23 +0500

    Delete MyWebsite directory

--- MyWebsite/blog/views.py
+++
@@ -1,7 +0,0 @@
-# Create your views here.
-from django.shortcuts import render
-from .models import Post
-
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'posts': posts})
